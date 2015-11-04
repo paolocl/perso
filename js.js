19f2b1d6c18a7c600d968d86add774a5cd82b1d8
paolocl <paolo.castro_lemaitre@yahoo.fr>
2015-11-04 09:51:38 +0100

    Create js.js

---
+++ js.js
@@ -0,0 +1,11 @@
+# perso
+perso
+		
+		 bur.addEventListener('click',function(){
+        if(menu_vertical.style.left === "0";){
+				menu_vertical.style.left = "-70%";
+        vierge.style.right= "-31%";}
+				else{
+				menu_vertical.style.left = "0";
+        vierge.style.right= "0"; }
+			},false);
